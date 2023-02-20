2e85ad584ebafc021f8cf352c31d705d4cfb6f4a
praje <prajesh.patel293@gmail.com>
2023-02-20 17:29:13 +0530

    home change

--- src/Components/Categories/Home.js
+++ src/Components/Categories/Home.js
@@ -19,40 +19,21 @@ export const Home = () => {
   } 
   return (
      <div>
-      
         <div className='mx-auto ' style={{width:700}}>        
-        
-          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-   
-    {
-        news.map((items) => ( 
-          <div class="carousel-item active">
-      <img src={items.yoast_head_json.og_image[0].url} class="d-block w-100" alt=""></img>
-      </div>
-      ))
-    }
-   
+       {
+        news.map((items) => (  
+          <div class="card" >
+  <img src={items.yoast_head_json.og_image[0].url} class="card-img-top" alt="..."></img> 
+  <div class="card-body">
+    <h4 class="card-title"><b>{items.title.rendered}</b></h4>
+    <p class="card-text">{items.excerpt.rendered}</p>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
-{/* //           <div class="card" >
-//   <img src={items.yoast_head_json.og_image[0].url} class="card-img-top" alt="..."></img> 
-//   <div class="card-body">
-//     <h4 class="card-title"><b>{items.title.rendered}</b></h4>
-//     <p class="card-text">{items.excerpt.rendered}</p>
-//   </div>
-// </div> */}
-         
-             
-        </div> <br>
+
+        ))
+       }      
+        </div>
+       <br>
         </br>
          <div aria-label="Page navigation example  ">
   <ul class="pagination justify-content-center">
